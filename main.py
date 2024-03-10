fbd8be0e8fe2acc857df45eea9689dd774cc3b5a
K  Eurekha <143890210+Eurekha-K@users.noreply.github.com>
2024-03-10 20:26:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -77,3 +77,5 @@ async def upload_image(image: UploadFile = File(...)):
         "detection_plot_url": detection_plot_url,
         "labels_with_confidence": labels
     })
+if __name__=="__main__":
+  uvicorn.run("main:app",host="127.0.0.1",port=8000,reload=True)
